78f15f726fd9e1712df6b33ece061bd541f86f8c
Facu Sichi <facundo.sichi@codetria.com>
2025-02-10 15:20:43 -0300

    Added Base Config & Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+
+export const App = () => {
+  return (
+    <section className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
+        <div className="">
+            App Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa
+            minima eos dolorem iure labore, quos assumenda eaque dicta asperiores
+            nostrum optio vero praesentium, dignissimos explicabo dolore pariatur
+            esse exercitationem odit mollitia, maxime ipsa? Magnam, obcaecati!
+            Accusamus commodi numquam, nulla deserunt molestias rerum voluptatem
+            praesentium illum perferendis similique tempore dignissimos officia,
+            iste hic alias voluptates aperiam sunt! Quaerat sequi ducimus quam
+            placeat iste provident! Aliquid, et assumenda. Corporis iste harum atque
+            facere maiores ea, doloremque ad aliquid id, minima alias fugit
+            voluptate iure earum? Voluptate atque velit nesciunt ad, laudantium
+            quibusdam expedita deserunt, eligendi ipsam error maxime commodi tempore
+            eaque inventore aut sunt. Sint deserunt nisi recusandae quibusdam velit?
+            Suscipit, delectus. In repudiandae ipsum nisi odit aut dolore iusto
+            quidem. Alias perspiciatis voluptatibus quo, saepe id aut! Obcaecati,
+            inventore soluta tempora exercitationem cum molestias ab facilis
+            officiis animi quibusdam iure enim, sed similique possimus aut?
+            Explicabo, sapiente laboriosam. Quas, facere quae perspiciatis odio,
+            itaque nulla temporibus odit incidunt expedita architecto, excepturi
+            fuga vero cumque? Neque iure enim consequatur inventore atque adipisci
+            obcaecati numquam exercitationem. Inventore voluptatum libero, expedita
+            officiis necessitatibus magni soluta error, doloremque pariatur
+            provident, iste deleniti labore? Hic expedita repudiandae saepe. Quasi
+            similique, amet quia temporibus perferendis repellat assumenda.
+        </div>
+  </section>
+  )
+}
---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import svgrPlugin from "vite-plugin-svgr";
+import tailwindcss from "@tailwindcss/vite";
+
+// import dns from 'dns';
+// dns.setDefaultResultOrder('verbatim');
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin(), tailwindcss()],
+  envDir: "./env",
+  server: {
+    port: 4000,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
+})
