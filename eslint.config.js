aee625f934ab3313ea6dc61da9fdb7b84c5a0783
Facu Sichi <facundo.sichi@codetria.com>
2025-02-10 16:28:28 -0300

    Added EsLint & Prettier Config

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import configPrettier from "eslint-plugin-prettier";
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -17,12 +18,26 @@ export default tseslint.config(
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      quotes: "off",
+      "quote-props": "off",
+      semi: [1, "always", { omitLastInOneLineBlock: true }],
+      "no-unused-expressions": "off",
+      "no-unused-vars": "warn",
+      "no-use-before-define": "off",
+      "no-absolute-path": "off",
+      "@typescript-eslint/no-use-before-define": ["error", { typedefs: false }],
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
     },
   },
+  configPrettier,
 )
